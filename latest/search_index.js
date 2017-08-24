524c76b4d8935fd237ba7ddbebe169662118ca76
autodocs <autodocs>
2017-08-24 23:13:01 +0000

    build based on 7648ecf

---
+++ latest/search_index.js
@@ -0,0 +1,27 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "index.html#",
+    "page": "Home",
+    "title": "Home",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "index.html#RequirementVersions.minimum_requirement_versions-Tuple{Any}",
+    "page": "Home",
+    "title": "RequirementVersions.minimum_requirement_versions",
+    "category": "Method",
+    "text": "minimum_requirement_versions(package_name; package_directory = Pkg.dir(), skips = String[])\n\nAutomatically finds the minimum versions of required packages that will still allow your tests to pass. List any packages you want to skip in skips.\n\nMakes the assumption that if a certain profile of versions work, all profiles with versions greater or equal will also work.\n\n\n\n"
+},
+
+{
+    "location": "index.html#RequirementVersions.jl-1",
+    "page": "Home",
+    "title": "RequirementVersions.jl",
+    "category": "section",
+    "text": "Modules = [RequirementVersions]"
+},
+
+]}
