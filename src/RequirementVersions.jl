d6ad089f74b28843469e7961f751961d5ae59407
Brandon Taylor <brandon.taylor221@gmail.com>
2017-09-01 14:15:43 -0400

    safer copies

--- src/RequirementVersions.jl
+++ src/RequirementVersions.jl
@@ -2,6 +2,8 @@ module RequirementVersions
 
 include("pin.jl")
 
+package_name = "ChainRecursive"
+
 extract_requirements(args...) = begin
     path = joinpath(args...)
     if ispath(path)
@@ -30,15 +32,18 @@ minimum_requirement_versions(package_name; package_directory = Pkg.dir(), skips 
         extract_requirements(package_file, "REQUIRE"),
         extract_requirements(package_file, "test", "REQUIRE")
     ), union(skips, ["julia"]))
-
+    archive = mktempdir(package_directory)
+    info("Making archive folder $archive to archive your pacakges. If anything goes wrong please run `restore($archive)`")
     version_numbers = map(requirements) do requirement
+        info("Archiving $requirement")
+        cp(joinpath(package_directory, requirement), joinpath(archive, requirement))
         versions = Pkg.available(requirement)
         while length(versions) > 1
             try
                 previous_version = versions[end - 1]
                 info("Downgrading to $requirement $previous_version")
                 my_pin(requirement, previous_version, should_resolve = false)
-                my_test(package_name, should_resolve = false)
+                my_test(package_name, should_resolve = false, coverage = false)
                 pop!(versions)
             catch
                 break
@@ -48,8 +53,21 @@ minimum_requirement_versions(package_name; package_directory = Pkg.dir(), skips 
         my_pin(requirement, last_version, should_resolve = false)
         last_version
     end
-    Pkg.free.(requirements)
+    restore(archive, package_directory)
     Dict(zip(requirements, version_numbers))
+end
+
+"""
+    restore(archive, package_directory = Pkg.dir())
+
+Restore all the files in the archive to your package directory.
+"""
+restore(archive, package_directory = Pkg.dir()) = begin
+    foreach(readdir(archive)) do file
+        dest = joinpath(package_directory, file)
+        cp(joinpath(archive, file), joinpath(package_directory, file), remove_destination = true)
+    end
+    rm(archive, recursive = true)
 end
 
 end
